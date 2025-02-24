b7c990d1960641047b06a83f7e7f0870c69d4bb6
JuHyeonLee <ljh3900@gmail.com>
2025-02-24 09:17:09 +0200

    restart

--- so_long/main.c
+++ so_long/main.c
@@ -3,60 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juhyeonl <juhyeonl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: JuHyeon <juhyeonl@student.hive.fi>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/15 17:45:26 by juhyeonl          #+#    #+#             */
-/*   Updated: 2025/02/18 15:52:12 by juhyeonl         ###   ########.fr       */
+/*   Created: 2025/02/23 21:11:48 by JuHyeon           #+#    #+#             */
+/*   Updated: 2025/02/23 21:14:22 by JuHyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	load_images(t_game *game)
+static void	load_images(t_game *game, int *img_w, int *img_h)
 {
-	int	img_w;
-	int	img_h;
-
-	game->img_wall = mlx_xpm_file_to_image(game->mlx, "./images/wall.xpm",
-			&img_w, &img_h);
-	game->img_player = mlx_xpm_file_to_image(game->mlx, "./images/chara.xpm",
-			&img_w, &img_h);
-	game->img_collectible_before = mlx_xpm_file_to_image(game->mlx, "./images/chest.xpm",
-			&img_w, &img_h);
-	game->img_collectible_after = mlx_xpm_file_to_image(game->mlx, "./images/chest_open.xpm",
-			&img_w, &img_h);
-	game->img_exit_close = mlx_xpm_file_to_image(game->mlx, "./images/rune.xpm",
-			&img_w, &img_h);
-	game->img_exit_open = mlx_xpm_file_to_image(game->mlx, "./images/rune_light.xpm",
-			&img_w, &img_h);
-	game->img_floor = mlx_xpm_file_to_image(game->mlx, "./images/land.xpm",
-			&img_w, &img_h);
-	if (!game->img_wall || !game->img_player || !game->img_collectible_after
-		|| !game->img_collectible_before || !game->img_exit_close
+	game->img_wall = mlx_xpm_file_to_image(game->mlx, \
+									"./images/wall.xpm", img_w, img_h);
+	game->img_player = mlx_xpm_file_to_image(game->mlx, \
+									"./images/chara.xpm", img_w, img_h);
+	game->img_collectible_before = mlx_xpm_file_to_image(game->mlx, \
+									"./images/chest.xpm", img_w, img_h);
+	game->img_collectible_after = mlx_xpm_file_to_image(game->mlx, \
+									"./images/chest_open.xpm", img_w, img_h);
+	game->img_exit_close = mlx_xpm_file_to_image(game->mlx, \
+									"./images/rune.xpm", img_w, img_h);
+	game->img_exit_open = mlx_xpm_file_to_image(game->mlx, \
+									"./images/rune_light.xpm", img_w, img_h);
+	game->img_floor = mlx_xpm_file_to_image(game->mlx, \
+									"./images/land.xpm", img_w, img_h);
+	if (!game->img_wall || !game->img_player || !game->img_collectible_before
+		|| !game->img_collectible_after || !game->img_exit_close
 		|| !game->img_exit_open || !game->img_floor)
 	{
-		ft_putstr_fd("Error: Failed to load one or more images\n", 2);
+		ft_putstr_fd("Error: Failed to load images\n", 2);
 		exit(1);
 	}
 }
 
-void	init_map(t_game *game, char *map_file)
-{
-	game->map = read_map(map_file, &game->width, &game->height);
-	if (!game->map)
-	{
-		ft_putstr_fd("Error: Failed to read the map\n", 2);
-		exit(1);
-	}
-	if (!validate_map(game))
-	{
-		ft_putstr_fd("Error: Invalid map\n", 2);
-		free_map(game->map, game->height);
-		exit(1);
-	}
-}
-
-void	init_window(t_game *game)
+static void	init_mlx_window(t_game *game)
 {
 	game->mlx = mlx_init();
 	if (!game->mlx)
@@ -64,8 +45,8 @@ void	init_window(t_game *game)
 		ft_putstr_fd("Error: Failed to initialize MiniLibX\n", 2);
 		exit(1);
 	}
-	game->win = mlx_new_window(game->mlx, game->width * TILE_SIZE,
-			game->height * TILE_SIZE, "so_long");
+	game->win = mlx_new_window(game->mlx, game->width * TILE_SIZE, \
+	game->height * TILE_SIZE, "so_long");
 	if (!game->win)
 	{
 		ft_putstr_fd("Error: Failed to create window\n", 2);
@@ -73,9 +54,25 @@ void	init_window(t_game *game)
 	}
 }
 
+static void	init_game(t_game *game, char *map_file)
+{
+	int	img_w;
+	int	img_h;
+
+	game->map = read_map(map_file, &game->width, &game->height);
+	if (!game->map || !validate_map(game))
+	{
+		ft_putstr_fd("Error: Invalid map\n", 2);
+		free_map(game->map, game->height);
+		exit(1);
+	}
+	init_mlx_window(game);
+	load_images(game, &img_w, &img_h);
+}
+
 int	handle_exit(t_game *game)
 {
-    (void)game;
+	free_map(game->map, game->height);
 	exit(0);
 	return (0);
 }
@@ -89,9 +86,7 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Error: Please provide a map file (.ber)\n", 2);
 		return (1);
 	}
-	init_map(&game, argv[1]);
-	init_window(&game);
-	load_images(&game);
+	init_game(&game, argv[1]);
 	render_map(&game);
 	game.moves = 0;
 	write(1, "moves : ", 8);
