8813b50b9372ee85c6fd34152e29c2297fe4a2da
Lee Ju Hyeon <ljh3900@gmail.com>
2025-02-27 01:56:15 +0200

    merge

---
+++ pipex/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhyeonl <juhyeonl@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 23:59:17 by juhyeonl          #+#    #+#             */
+/*   Updated: 2025/02/25 23:59:17 by juhyeonl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <errno.h>
+# include <string.h>
+
+#endif
